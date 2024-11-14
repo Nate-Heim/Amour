beecb5ab95f3399eb938d933bdeba5fed3ef5f6a
ColinConrad2 <aceofspadescolin@gmail.com>
2024-11-14 14:17:32 -0700

    colins basic home template and setup

---
+++ publicPages/views.py
@@ -0,0 +1,8 @@
+#Amour/publicPages/views.py
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = "home.html"
+
+# Create your views here.
