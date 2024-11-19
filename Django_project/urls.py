4f0d758106841d64adf984f635133be1093f64f5
ColinConrad2 <aceofspadescolin@gmail.com>
2024-11-19 13:34:22 -0700

    django toolbar

--- Django_project/urls.py
+++ Django_project/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 #Amour/Django_project/urls.py
 from django.contrib import admin
 from django.urls import path, include
+import debug_toolbar 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("publicPages.urls")),
     path("accounts/", include("accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
-]
+    path("_debug_/", include(debug_toolbar.urls)),
+] 
