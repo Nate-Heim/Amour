81d27c6c61da67812543b9ad0a94a1b8af4855f5
Colin Conrad <aceofspadescolin@gmail.com>
2024-12-01 20:28:08 -0700

    password change/reset & slight design tweaks

--- Django_project/urls.py
+++ Django_project/urls.py
@@ -19,6 +19,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, re_path
 import debug_toolbar
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,11 @@
 #Amour/accounts/urls.py
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from .views import SignUpView
 
 urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(), name='login'),  # Define the login view
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name="registration/password_reset_form.html"), name='password_reset'),
     path("signup/", SignUpView.as_view(), name="signup"),
     path("captcha/", include("captcha.urls")),
 ]
